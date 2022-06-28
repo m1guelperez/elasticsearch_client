6bee67437717e71fffe0d943265d9bc5d6a9b0e9
m1guelperez <migi.perez@web.de>
2022-06-28 23:59:12 +0200

    Created base- and first subclass. || Created wildcard query.

--- utilities/querybuilder/queries.cpp
+++ utilities/querybuilder/queries.cpp
@@ -5,26 +5,47 @@
 #include "queries.h"
 
 #include <string>
+#include <iostream>
+#include <memory>
 
-QueryBuilder QueryBuilder::match(const std::string& key, const std::string& value) {
-    this->currentQuery = "{\n"
-                         "  \"query\": {\n"
-                         "    \"match\": {\n"
-                         "      \"" + key + "\": " + "\"" + value + "\"\n" +
-                         "    }\n"
-                         "  }\n"
-                         "}";
-    return *this;
+// Creates a ptr to a created subclass containing a wildcard request
+std::unique_ptr<Query> QueryBuilder::wildcard(const std::string &field, const std::string &value) {
+    std::unique_ptr<Query> query(new Query);
+    std::string wildcardQuery = "\"wildcard\": {\n "
+                                "  \"" + field + "\": {\n"
+                                                 "    \"value\":" + "\"" + value + "\"";
+    query->currentQuery = currentQuery.append(wildcardQuery);
+    query->incrementQueryDepth(2);
+    return query;
 }
 
-QueryBuilder QueryBuilder::wildcard() {
-    return *this;
+std::string QueryBuilder::getQuery() {
+    return currentQuery;
 }
 
-std::string QueryBuilder::getCurrentQuery() {
+void QueryBuilder::incrementQueryDepth(int inc) {
+    this->queryDepth = queryDepth + inc;
+}
+
+int QueryBuilder::getQueryDepth() const {
+    return queryDepth;
+}
+
+//Will check the number of nested statements and close them. Results in a built string.
+std::string QueryBuilder::build() {
+    for (int i = 0; i < this->queryDepth; ++i) {
+        this->currentQuery.append("\n}");
+    }
     return this->currentQuery;
 }
 
-void QueryBuilder::resetQuery() {
-    this->currentQuery = "";
+//TODO: Continue finishing bool queries
+Query Query::boolQuery() {
+    if (this->queryDepth > 2) {
+        this->currentQuery.append(", \n \" bool\": {\n");
+    } else {
+        this->currentQuery.append("\n \" bool\": {\n");
+
+    }
+    return *this;
 }
--- utilities/querybuilder/queries.h
+++ utilities/querybuilder/queries.h
@@ -5,18 +5,37 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
+class QueryBuilder;
+
+class Query;
 /**
  * This will be the Querybuilder similar to the Python elasticsearch_dsl package
  */
+
+//Base class
 class QueryBuilder {
-    std::string currentQuery;
+private:
+    std::string currentQuery = "{\n \"query\": {\n ";
+    int queryDepth = 2;
+    friend class Query;
 public:
-    QueryBuilder match(const std::string &key, const std::string &value);
+    std::unique_ptr<Query> wildcard(const std::string& field, const std::string& value);
 
-    QueryBuilder wildcard();
+    std::string getQuery();
 
-    std::string getCurrentQuery();
+    void incrementQueryDepth(int inc);
 
-    void resetQuery();
+    int getQueryDepth() const;
+
+    std::string build();
+};
+
+// First inheritance
+class Query : public QueryBuilder {
+
+private:
+public:
+    Query boolQuery();
 };
