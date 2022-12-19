3a473a70292f6ca89857ca46cb08d6f589d4c57f
m1guelperez <migi.perez@web.de>
2022-12-19 23:27:27 +0100

    Added new Builder-Approach.

---
+++ utilities/querybuilder/searchQuery.cpp
@@ -0,0 +1,5 @@
+//
+// Created by miguel on 12/19/22.
+//
+
+#include "searchQuery.h"
---
+++ utilities/querybuilder/searchQuery.h
@@ -0,0 +1,8 @@
+//
+// Created by miguel on 12/19/22.
+//
+
+#ifndef ELASTIC_SEARCHQUERY_H
+#define ELASTIC_SEARCHQUERY_H
+
+#endif //ELASTIC_SEARCHQUERY_H
